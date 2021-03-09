aee85b4513d515d87e17e568e8fb06b8de2b4aa4
theLuis6827 <lozano_22_lalo@yahoo.com>
2021-03-09 14:05:59 -0600

    Review done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 1) do
+ActiveRecord::Schema.define(version: 2) do
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "song_id"
+    t.integer "user_id"
+    t.integer "rating"
+    t.string "phrase"
+  end
 
   create_table "songs", force: :cascade do |t|
     t.string "name"
